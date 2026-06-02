bf03323705b2e2fee64fbba967dca8d32c03c64a
Luiz Pizano Fonseca <luizpizano.lpf@gmail.com>
2026-06-02 20:11:37 -0300

    scala implicits: remove comments from source

--- scala/Implicits.scala
+++ scala/Implicits.scala
@@ -1,6 +1,3 @@
-// --- IMPLICITS (Scala 3 style: given/using) ---
-
-// 1. Type class pattern: define a type class and instances
 trait Show[A]:
   def show(a: A): String
 
@@ -15,26 +12,21 @@ given Show[Boolean] with
 
 def display[A](a: A)(using s: Show[A]): String = s.show(a)
 
-// 2. Implicit conversion: wrap an Int as a "rich" type
 case class Celsius(value: Double):
   def toFahrenheit: Double = value * 9.0 / 5.0 + 32
 
 given Conversion[Double, Celsius] = Celsius(_)
 
-
 @main def run(): Unit =
-  // Type class dispatch
   println("Type class (Show):")
   println(s"  ${display(42)}")
   println(s"  ${display("hello")}")
   println(s"  ${display(true)}")
 
-  // Implicit conversion
   println("\nImplicit conversion (Double -> Celsius):")
   val temp: Celsius = 100.0
   println(s"  100.0°C = ${temp.toFahrenheit}°F")
 
-  // Summoning a given directly with summon
   println("\nSummoning a given:")
   val showInt = summon[Show[Int]]
   println(s"  ${showInt.show(99)}")
