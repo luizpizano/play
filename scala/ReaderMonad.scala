162bec79e84cf9aa4591c2e0cd372b749c3fa541
Luiz Pizano Fonseca <luizpizano.lpf@gmail.com>
2026-06-15 18:45:12 -0300

    scala Reader monad: usage demo (Config, connStr, jdbcUrl)

--- scala/ReaderMonad.scala
+++ scala/ReaderMonad.scala
@@ -7,3 +7,15 @@ object Reader:
   def ask[R]: Reader[R, R]                                    = Reader(identity)
   def asks[R, A](f: R => A): Reader[R, A]                    = Reader(f)
   def local[R, A](f: R => R)(ra: Reader[R, A]): Reader[R, A] = Reader(r => ra.run(f(r)))
+
+case class Config(host: String, port: Int, db: String)
+
+def dbHost: Reader[Config, String] = Reader.asks(_.host)
+def dbPort: Reader[Config, Int]    = Reader.asks(_.port)
+def dbName: Reader[Config, String] = Reader.asks(_.db)
+
+def connStr: Reader[Config, String] =
+  for h <- dbHost; p <- dbPort yield s"$h:$p"
+
+def jdbcUrl: Reader[Config, String] =
+  for conn <- connStr; db <- dbName yield s"jdbc:postgresql://$conn/$db"
