df68f661b54da4c97e7ed9db1a8602ebeb9e566e
Luiz Pizano Fonseca <luizpizano.lpf@gmail.com>
2026-06-23 19:05:46 -0300

    scala Writer monad: plain version comparison (log threaded manually)

--- scala/WriterMonad.scala
+++ scala/WriterMonad.scala
@@ -7,3 +7,11 @@ final class Writer[W, A](val run: (W, A))(using W: Monoid[W]):
   def flatMap[B](f: A => Writer[W, B]): Writer[W, B] =
     val (w2, b) = f(run._2).run
     Writer((W.combine(run._1, w2), b))
+
+given Monoid[String] with
+  def empty = ""
+  def combine(x: String, y: String) = x + y
+
+def addPlain(x: Int, y: Int): (String, Int) =
+  val sum = x + y
+  (s"added $x + $y = $sum\n", sum)
